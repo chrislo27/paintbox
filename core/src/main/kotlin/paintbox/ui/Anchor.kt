80d17f116ff4c237633cd442805fe626477c2289
chrislo27 <kinchrislo@gmail.com>
2022-03-10 14:25:43 -0800

    Refactor Anchor: configure functions now per axis, no public API changes

--- core/src/main/kotlin/paintbox/ui/Anchor.kt
+++ core/src/main/kotlin/paintbox/ui/Anchor.kt
@@ -2,7 +2,13 @@ package paintbox.ui
 
 import paintbox.binding.Var
 
-
+/**
+ * The anchor system provides utilities for setting/binding the dimensional [bounds][UIElement.bounds]
+ * properties. Nested sub-classes inside [Anchor] implement various anchor types, like centering and right alignment. 
+ * 
+ * The [configure] functions let you configure both the x and y dimensions at the same time.
+ * If only one dimension at a time has to be edited, then the [xConfigure] and [yConfigure] functions can be used.
+ */
 sealed class Anchor {
 
     /**
@@ -10,16 +16,21 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object TopLeft : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             element.bounds.x.set(offsetX)
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
             element.bounds.y.set(offsetY)
         }
         
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             element.bounds.x.bind {
                 offsetX.invoke(this)
             }
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
             element.bounds.y.bind {
                 offsetY.invoke(this)
             }
@@ -31,20 +42,25 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object CentreLeft : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
-            val parent = element.parent
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             element.bounds.x.set(offsetX)
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) / 2f - (element.bounds.height.use() / 2f) + offsetY
             }
         }
 
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
-            val parent = element.parent
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             element.bounds.x.bind {
                 offsetX.invoke(this)
             }
+        }
+
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) / 2f - (element.bounds.height.use() / 2f) + offsetY.invoke(this)
             }
@@ -56,20 +72,25 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object BottomLeft : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
-            val parent = element.parent
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             element.bounds.x.set(offsetX)
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) - (element.bounds.height.use()) + offsetY
             }
         }
 
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
-            val parent = element.parent
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             element.bounds.x.bind {
                 offsetX.invoke(this)
             }
+        }
+
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use()
                         ?: 0f) - (element.bounds.height.use()) + offsetY.invoke(this)
@@ -82,20 +103,25 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object TopRight : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) - (element.bounds.width.use()) + offsetX
             }
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
             element.bounds.y.set(offsetY)
         }
 
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) - (element.bounds.width.use()) + offsetX.invoke(this)
             }
+        }
+
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
             element.bounds.y.bind {
                 offsetY.invoke(this)
             }
@@ -107,22 +133,29 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object CentreRight : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) - (element.bounds.width.use()) + offsetX
             }
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) / 2f - (element.bounds.height.use() / 2f) + offsetY
             }
         }
 
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) - (element.bounds.width.use()) + offsetX.invoke(this)
             }
+        }
+
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) / 2f - (element.bounds.height.use() / 2f) + offsetY.invoke(this)
             }
@@ -134,22 +167,29 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object BottomRight : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) - (element.bounds.width.use()) + offsetX
             }
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) - (element.bounds.height.use()) + offsetY
             }
         }
 
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) - (element.bounds.width.use()) + offsetX.invoke(this)
             }
+        }
+
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use()
                         ?: 0f) - (element.bounds.height.use()) + offsetY.invoke(this)
@@ -162,20 +202,25 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object TopCentre : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) / 2f - (element.bounds.width.use() / 2f) + offsetX
             }
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
             element.bounds.y.set(offsetY)
         }
 
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) / 2f - (element.bounds.width.use() / 2f) + offsetX.invoke(this)
             }
+        }
+
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
             element.bounds.y.bind {
                 offsetY.invoke(this)
             }
@@ -187,22 +232,29 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object BottomCentre : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) / 2f - (element.bounds.width.use() / 2f) + offsetX
             }
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) - (element.bounds.height.use()) + offsetY
             }
         }
         
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) / 2f - (element.bounds.width.use() / 2f) + offsetX.invoke(this)
             }
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use()
                         ?: 0f) - (element.bounds.height.use()) + offsetY.invoke(this)
@@ -215,22 +267,29 @@ sealed class Anchor {
      * Positive X offset is further RIGHT, positive Y offset is further DOWN.
      */
     object Centre : Anchor() {
-        override fun configure(element: UIElement, offsetX: Float, offsetY: Float) {
+        override fun xConfigure(element: UIElement, offsetX: Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) / 2f - (element.bounds.width.use() / 2f) + offsetX
             }
+        }
+        
+        override fun yConfigure(element: UIElement, offsetY: Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) / 2f - (element.bounds.height.use() / 2f) + offsetY
             }
         }
 
-        override fun configure(element: UIElement, offsetX: Var.Context.() -> Float,
-                               offsetY: Var.Context.() -> Float) {
+        override fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float) {
             val parent = element.parent
             element.bounds.x.bind {
                 (parent.use()?.contentZone?.width?.use() ?: 0f) / 2f - (element.bounds.width.use() / 2f) + offsetX.invoke(this)
             }
+        }
+
+        override fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float) {
+            val parent = element.parent
             element.bounds.y.bind {
                 (parent.use()?.contentZone?.height?.use() ?: 0f) / 2f - (element.bounds.height.use() / 2f) + offsetY.invoke(this)
             }
@@ -238,22 +297,56 @@ sealed class Anchor {
     }
 
     /**
+     * Configures the [element] based on this [Anchor], only on the X axis.
+     */
+    abstract fun xConfigure(element: UIElement, offsetX: Float)
+    
+    /**
+     * Configures the [element] based on this [Anchor] with bindable offsets to the bounds x [Var] context.
+     *
+     * The context in the [offsetX] function parameters is the [Var.Context] of the bounds x [Var].
+     */
+    abstract fun xConfigure(element: UIElement, offsetX: Var.Context.() -> Float)
+
+    /**
+     * Configures the [element] based on this [Anchor], only on the X axis.
+     */
+    abstract fun yConfigure(element: UIElement, offsetY: Float)
+    
+    /**
+     * Configures the [element] based on this [Anchor] with bindable offsets to the bounds y [Var] context.
+     *
+     * The context in the [offsetY] function parameters is the [Var.Context] of the bounds y [Var].
+     */
+    abstract fun yConfigure(element: UIElement, offsetY: Var.Context.() -> Float)
+
+
+    /**
      * Configures the [element] based on this [Anchor] with constant offsets. 
      */
-    abstract fun configure(element: UIElement, offsetX: Float = 0f, offsetY: Float = 0f)
+    @Suppress("NOTHING_TO_INLINE", "RedundantModalityModifier")
+    final inline fun configure(element: UIElement, offsetX: Float = 0f, offsetY: Float = 0f) {
+        xConfigure(element, offsetX)
+        yConfigure(element, offsetY)
+    }
     
     /**
      * Configures the [element] based on this [Anchor] with bindable offsets to the bounds x/y [Var] context.
      * 
      * The context in the [offsetX]/[offsetY] function parameters is the [Var.Context] of the bounds x/y [Var].
      */
-    abstract fun configure(element: UIElement,
-                           offsetX: Var.Context.() -> Float = { 0f },
-                           offsetY: Var.Context.() -> Float = { 0f })
+    @Suppress("NOTHING_TO_INLINE", "RedundantModalityModifier")
+    final inline fun configure(element: UIElement,
+                               noinline offsetX: Var.Context.() -> Float = { 0f },
+                               noinline offsetY: Var.Context.() -> Float = { 0f }) {
+        xConfigure(element, offsetX)
+        yConfigure(element, offsetY)
+    }
     
     /**
      * Configures the [element] based on this [Anchor] with constant offsets of 0.
      */
-    final fun configure(element: UIElement) = configure(element, 0f, 0f)
+    @Suppress("NOTHING_TO_INLINE", "RedundantModalityModifier")
+    final inline fun configure(element: UIElement) = configure(element, 0f, 0f)
 
 }
