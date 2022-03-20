db1e212a58d49e5c7c8fd1f98945d45de27ddfc3
chrislo27 <kinchrislo@gmail.com>
2022-03-19 19:52:15 -0700

    Update documentation regarding listener firing

--- utils/src/main/kotlin/paintbox/binding/VarChangedListener.kt
+++ utils/src/main/kotlin/paintbox/binding/VarChangedListener.kt
@@ -2,7 +2,11 @@ package paintbox.binding
 
 
 /**
- * A listener that is fired when a [ReadOnlyVar] is changed.
+ * A listener that is fired when a [ReadOnlyVar] is changed or invalidated.
+ * 
+ * Listeners are not re-fired if they were previously fired on an invalidation event.
+ * Specifically, the compute-type bindings do not trigger listeners when they are *actually* computed, just only when
+ * their internal value has been invalidated.
  */
 fun interface VarChangedListener<in T> {
     fun onChange(v: ReadOnlyVar<T>)
@@ -14,7 +18,7 @@ fun interface VarChangedListener<in T> {
 interface DisposableVarChangedListener<in T> : VarChangedListener<T> {
     /**
      * True when this listener should be disposed.
-     * Once this is true, it should never be false on subsequent gets.
+     * Once this is true, it shall never be false on subsequent gets.
      */
     val shouldBeDisposed: Boolean
 }
