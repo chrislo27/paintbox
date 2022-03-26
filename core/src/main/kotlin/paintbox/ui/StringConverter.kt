f1c435c5cfb96e9f9caecda86abab950db17e8b9
chrislo27 <kinchrislo@gmail.com>
2022-03-26 11:52:47 -0700

    Add StringVarConverter and extension function for StringConverter

--- core/src/main/kotlin/paintbox/ui/StringConverter.kt
+++ core/src/main/kotlin/paintbox/ui/StringConverter.kt
@@ -1,14 +1,33 @@
 package paintbox.ui
 
+import paintbox.binding.ReadOnlyVar
+import paintbox.ui.StringConverter.Companion
 
+
+/**
+ * A functional interface that converts an object [T] into a string.
+ * 
+ * The default implementation ([Companion.DEFAULT_STRING_CONVERTER]) simply calls [toString].
+ */
 fun interface StringConverter<T> {
+    
     companion object {
         val DEFAULT_STRING_CONVERTER: StringConverter<Any?> = StringConverter { it.toString() }
     }
     
-    
     fun convert(item: T): String
     
-    operator fun invoke(item: T): String = convert(item) // Code-compatibility
+}
+
+/**
+ * A functional interface that converts an object [T] into a string [ReadOnlyVar].
+ */
+fun interface StringVarConverter<T> {
+
+    fun toVar(item: T): ReadOnlyVar<String>
     
 }
+
+fun <T> StringConverter<T>.toVarConverter(): StringVarConverter<T> = StringVarConverter { item: T ->
+    ReadOnlyVar.const(this.convert(item))
+} 
