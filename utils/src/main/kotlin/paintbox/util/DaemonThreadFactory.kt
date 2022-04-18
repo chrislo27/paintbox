9fbc13ce1fbd1f3d7bf84b2784a4a473aae92989
chrislo27 <kinchrislo@gmail.com>
2022-04-18 14:54:22 -0700

    Add DaemonThreadFactory

---
+++ utils/src/main/kotlin/paintbox/util/DaemonThreadFactory.kt
@@ -0,0 +1,16 @@
+package paintbox.util
+
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
+
+
+/**
+ * A [ThreadFactory] that uses [Executors.defaultThreadFactory] and makes its created threads daemon threads.
+ */
+class DaemonThreadFactory : ThreadFactory {
+    override fun newThread(r: Runnable): Thread {
+        return Executors.defaultThreadFactory().newThread(r).apply { 
+            this.isDaemon = true
+        }
+    }
+}
