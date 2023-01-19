61f7a0c989d9b6795aa16ae0a4188f26b84b77ce
chrislo27 <kinchrislo@gmail.com>
2023-01-18 21:26:07 -0800

    Add Color extensions for alpha and rgbToGreyscale

--- core/src/main/kotlin/paintbox/util/gdxutils/ColorUtils.kt
+++ core/src/main/kotlin/paintbox/util/gdxutils/ColorUtils.kt
@@ -9,3 +9,16 @@ fun Color.set(r: Int, g: Int, b: Int, a: Int = 255): Color {
     this.set(r / 255f, g / 255f, b / 255f, a / 255f)
     return this
 }
+
+fun Color.alpha(newAlpha: Float): Color {
+    this.a = newAlpha
+    return this
+}
+
+fun Color.rgbToGreyscale(): Color {
+    val y = (this.r * 0.2126f + this.g * 0.7152f + this.b * 0.0722f).coerceIn(0f, 1f)
+    this.r = y
+    this.g = y
+    this.b = y
+    return this
+}
