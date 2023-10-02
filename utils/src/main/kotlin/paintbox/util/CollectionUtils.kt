65841bf699ad5253f2030bc143432f7af996f59b
chrislo27 <kinchrislo@gmail.com>
2023-10-02 10:06:22 -0700

    Move single function from CollectionUtils to IterableUtils

--- utils/src/main/kotlin/paintbox/util/CollectionUtils.kt
+++
@@ -1,8 +0,0 @@
-package paintbox.util
-
-
-inline fun <reified I> Iterable<*>.filterAndIsInstance(predicate: (I) -> Boolean): List<I> {
-    val list = mutableListOf<I>()
-    for (element in this) if (element is I && predicate(element)) list.add(element)
-    return list
-}
--- utils/src/main/kotlin/paintbox/util/IterableUtils.kt
+++ utils/src/main/kotlin/paintbox/util/IterableUtils.kt
@@ -7,3 +7,9 @@ inline fun <T> Iterable<T>.sumOfFloat(selector: (T) -> Float): Float {
     }
     return sum
 }
+
+inline fun <reified I> Iterable<*>.filterAndIsInstance(predicate: (I) -> Boolean): List<I> {
+    val list = mutableListOf<I>()
+    for (element in this) if (element is I && predicate(element)) list.add(element)
+    return list
+}
