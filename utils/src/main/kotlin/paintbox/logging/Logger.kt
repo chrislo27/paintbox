84a81346204b7c2c0cafc9b7f0e5ea1cb66bca93
chrislo27 <kinchrislo@gmail.com>
2025-01-19 11:36:46 -0800

    Refactor Logger

--- utils/src/main/kotlin/paintbox/logging/Logger.kt
+++ utils/src/main/kotlin/paintbox/logging/Logger.kt
@@ -1,10 +1,29 @@
 package paintbox.logging
 
+import java.io.PrintStream
+
 
 open class Logger {
 
     enum class LogLevel(val levelNumber: Int) {
-        ALL(Int.MIN_VALUE), TRACE(-1), DEBUG(0), INFO(1), WARN(2), ERROR(3), NONE(Int.MAX_VALUE)
+        ALL(Int.MIN_VALUE),
+        TRACE(-1),
+        DEBUG(0),
+        INFO(1),
+        WARN(2),
+        ERROR(3),
+        NONE(Int.MAX_VALUE);
+        
+        fun getLogName(): String {
+            return when (this) {
+                TRACE -> "TRC"
+                DEBUG -> "DBG"
+                INFO -> "INF"
+                WARN -> "WRN"
+                ERROR -> "ERR"
+                else -> this.name
+            }
+        }
     }
 
     val msTimeStarted: Long = System.currentTimeMillis()
@@ -13,27 +32,27 @@ open class Logger {
 
     var loggingLevel: LogLevel = LogLevel.DEBUG
 
-    protected open fun defaultPrint(level: LogLevel, msg: String, tag: String, throwable: Throwable?) {
+    protected open fun getTimestamp(): String {
         val millis = msTimeElapsed
         val second = (millis / 1000) % 60
         val minute = millis / (1000 * 60) % 60
         val hour = millis / (1000 * 60 * 60) % 24
-        val text = "${
-            String.format(
-                "%02d:%02d:%02d.%03d",
-                hour,
-                minute,
-                second,
-                millis % 1000
-            )
-        }: [${level.name}][${Thread.currentThread().name}] ${if (tag.isEmpty()) "" else "[$tag] "}$msg"
+        
+        return String.format(
+            "%02d:%02d:%02d.%03d",
+            hour,
+            minute,
+            second,
+            millis % 1000
+        )
+    }
+    
+    protected open fun defaultPrint(level: LogLevel, msg: String, tag: String, throwable: Throwable?) {
+        val text = "${getTimestamp()}: [${level.getLogName()}][${Thread.currentThread().name}] ${if (tag.isEmpty()) "" else "[$tag] "}$msg"
 
-        if (level.ordinal >= LogLevel.WARN.ordinal) {
-            System.err.println(text)
-        } else {
-            @Suppress("ReplaceJavaStaticMethodWithKotlinAnalog")
-            System.out.println(text)
-        }
+        val printStream: PrintStream = if (level.ordinal >= LogLevel.WARN.ordinal) System.err else System.out
+        
+        printStream.println(text)
 
         throwable?.printStackTrace(System.err)
     }
@@ -43,8 +62,6 @@ open class Logger {
             defaultPrint(logLevel, msg, tag, throwable)
         }
     }
-
-    // Special methods for each log level
 
     fun trace(msg: String, tag: String = "", throwable: Throwable? = null) {
         log(LogLevel.TRACE, msg, tag, throwable)
@@ -65,48 +82,4 @@ open class Logger {
     fun error(msg: String, tag: String = "", throwable: Throwable? = null) {
         log(LogLevel.ERROR, msg, tag, throwable)
     }
-
-    // Optimized methods for each log level without going through defaults
-    // No throwable param: no tag and with tag.
-
-    fun trace(msg: String) {
-        log(LogLevel.TRACE, msg, "", null)
-    }
-
-    fun debug(msg: String) {
-        log(LogLevel.DEBUG, msg, "", null)
-    }
-
-    fun info(msg: String) {
-        log(LogLevel.INFO, msg, "", null)
-    }
-
-    fun warn(msg: String) {
-        log(LogLevel.WARN, msg, "", null)
-    }
-
-    fun error(msg: String) {
-        log(LogLevel.ERROR, msg, "", null)
-    }
-
-    fun trace(msg: String, tag: String) {
-        log(LogLevel.TRACE, msg, tag, null)
-    }
-
-    fun debug(msg: String, tag: String) {
-        log(LogLevel.DEBUG, msg, tag, null)
-    }
-
-    fun info(msg: String, tag: String) {
-        log(LogLevel.INFO, msg, tag, null)
-    }
-
-    fun warn(msg: String, tag: String) {
-        log(LogLevel.WARN, msg, tag, null)
-    }
-
-    fun error(msg: String, tag: String) {
-        log(LogLevel.ERROR, msg, tag, null)
-    }
-
 }
